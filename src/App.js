1076345e3c055607d47d5d7092d317d42bf9f21f
Eric Riviere <eric.riviere.ponsa@gmail.com>
2023-07-24 17:10:21 +0200

    segunda clase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./Header";
+//Primer componente
+
+/**COMPONENTES:
+ Reglas:
+1 - Son funciones
+2 - Inician en mayúscula
+3 - Todos tienen return
+4 - Siempre deben retornar un solo elemento (etiquetas html o otro componente)
+<></> Fragmentos: etiquetas que no salen en el código pero sirven para contener 
+                  varios elementos para devolverlos como 1
+ */
 
 function App() {
+  let nombre = "Eric";
+  //Vista
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola Mundo</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/*Los comentarios en JSX se hacen así*/}
+      {/**      <header id="header" className="header">
+        <h1>My Page</h1>
+      </header> */}
+      <Header />
+      <main>
+        <h2>Home</h2>
+        <p>Bienvenido {nombre}</p>
+      </main>
+      <footer>
+        <p>Copyright &copy;</p>
+      </footer>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header id="header" className="header">
+      <h1>My Page</h1>
+    </header>
+  );
+}
+
+export default Header;
